1a01e2765b742357023a40120e93711d15bdb305
Anthony Kluba <anthonykluba@fullsail.edu>
2015-03-10 18:48:13 -0400

    clean up Read

--- assets/js/ajax/read.js
+++ assets/js/ajax/read.js
@@ -1,49 +1,58 @@
-// start with js_individual_recipient click off
-$('.js_individual_recipient').off('click');
-$( "#js_view_overview" ).addClass( 'current_analyze_type' );
-$( ".recipient_item" ).addClass( 'recipient_hidden' );
+$(document).ready(function(){ 
 
-
-// onclick js_view_individual click on
-$('#js_view_individual').on('click', function(){
+	/*
+	** Analyzing Individual
+	*********************************************** */
 	
-	$( "#js_view_overview" ).removeClass( 'current_analyze_type' );
-	$( ".recipient_item" ).removeClass( 'recipient_hidden' );
-	$( "#js_view_individual" ).addClass( 'current_analyze_type' );
-	
-	
-	$('.js_individual_recipient').on('click',function(){
-		var chosen_recipient = this;
-		// get view with question data to edit
-		$.ajax({
-			type: "GET",
-			url: "../survey_analyze/individual/" + $(this).data('id'),
-			success: function(res){
-				/* -------------------------------
-					APPENDING INDIVIDUAL RESULTS
-				------------------------------- */
-				// empty existing div
-				$('section').empty();
-				// append view from ajax call
-				$('section').append(res);
-				
-				$('.recipient_item').removeClass('current_recipient_item');
-				$(chosen_recipient).addClass('current_recipient_item');
-				
-				$('#js_view_overview').on('click',function(){
-					location.reload();
-				});
-				
-			}// end of success
-		});// end ajax()
-	});// end click individual recipient
-});// end click individual type
-
-$('#js_view_overview').on('click', function(){
+	// start with js_individual_recipient click off
 	$('.js_individual_recipient').off('click');
-	$( "#js_view_individual" ).removeClass( 'current_analyze_type' );
-	$('.recipient_item').removeClass('current_recipient_item');
 	$( "#js_view_overview" ).addClass( 'current_analyze_type' );
 	$( ".recipient_item" ).addClass( 'recipient_hidden' );
-});
-
+	
+	// onclick js_view_individual click on
+	$('#js_view_individual').on('click', function(){
+		// visually switch from overview to individual
+		$( "#js_view_overview" ).removeClass( 'current_analyze_type' );
+		$( ".recipient_item" ).removeClass( 'recipient_hidden' );
+		$( "#js_view_individual" ).addClass( 'current_analyze_type' );
+		
+		// on click of an individual
+		$('.js_individual_recipient').on('click',function(){
+			var chosen_recipient = this;
+			// get view with question data to edit
+			$.ajax({
+				type: "GET",
+				url: "../survey_analyze/individual/" + $(this).data('id'),
+				success: function(res){
+					/* -------------------------------
+						APPENDING INDIVIDUAL RESULTS
+					------------------------------- */
+					// empty existing div
+					$('section').empty();
+					// append view from ajax call
+					$('section').append(res);
+					
+					$('.recipient_item').removeClass('current_recipient_item');
+					$(chosen_recipient).addClass('current_recipient_item');
+					
+					$('#js_view_overview').on('click',function(){
+						location.reload();
+					});
+					
+				}// end of success
+			});// end ajax()
+		});// end click individual recipient
+		
+	});// end click individual type
+	
+	// switching back to overview
+	$('#js_view_overview').on('click', function(){
+		$('.js_individual_recipient').off('click');
+		$( "#js_view_individual" ).removeClass( 'current_analyze_type' );
+		$('.recipient_item').removeClass('current_recipient_item');
+		$( "#js_view_overview" ).addClass( 'current_analyze_type' );
+		$( ".recipient_item" ).addClass( 'recipient_hidden' );
+	});
+	
+	
+});// end doc ready()
